218f94b0ffecaf65d3989b7d9b30f6bd55ca5eb5
Dagaroth3exe <parasharrishabh1503@gmail.com>
2025-12-24 14:37:44 +0530

    added responsive recipe cards layout

--- src/Pages/RecipePage.jsx
+++ src/Pages/RecipePage.jsx
@@ -15,12 +15,15 @@ const RecipePage =()=>{
             <h1 className="pb-5 text-xl sm:text-2xl font-bold">Take your Pick</h1>
 
             <div className="mb-16 cardContainer flex flex-col sm:flex-row flex-wrap justify-center gap-7 ">
-                <div className="RecipeCard1 bg-[#FF3131] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out
-                hover:-translate-y-2 hover:shadow-2xl"></div>
-                <div className="RecipeCard2 bg-[#3179FF] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out
-                hover:-translate-y-2 hover:shadow-2xl"></div>
-                <div className="RecipeCard3 bg-[#E3FF31] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out
-                hover:-translate-y-2 hover:shadow-2xl"></div>
+                <div className="RecipeCard1 bg-[#FF3131] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl flex items-center justify-center">
+                    <div className="h-36 w-72 bg-[#4B0000] p-10 rounded-2xl -translate-y-[105px]"></div>
+                </div>
+                <div className="RecipeCard2 bg-[#3179FF] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl flex items-center justify-center">
+                    <div className="h-36 w-72 bg-[#000A4B] p-10 rounded-2xl -translate-y-[105px]"></div>
+                </div>
+                <div className="RecipeCard3 bg-[#E3FF31] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl flex items-center justify-center">
+                    <div className="h-36 w-72 bg-[#464B00] p-10 rounded-2xl -translate-y-[105px]">Some Recipe</div>
+                </div>
             </div>
             
         </div>
