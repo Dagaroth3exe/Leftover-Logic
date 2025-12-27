6a4aea9bd105e0e43f7978751bdb4ac1738522c9
Dagaroth3exe <parasharrishabh1503@gmail.com>
2025-12-27 17:30:28 +0530

    refined recipeCard Buttons

--- src/Pages/RecipePage.jsx
+++ src/Pages/RecipePage.jsx
@@ -11,7 +11,7 @@ const RecipePage = () => {
 
   return (
     <>
-      <div className="min-h-screen bg-blue-200 flex items-center justify-center scroll-m-0 flex-col">
+      <div className="p-4 h-screen bg-blue-200 flex items-center justify-center flex-col overflow-hidden">
         <img
           src={Logo}
           alt="Company Logo"
@@ -27,7 +27,7 @@ const RecipePage = () => {
                 {recipes[0].title}
               </h1>
             </div>
-            <button className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-black h-12 w-72 rounded-xl font-semibold">Explore</button>
+            <button className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-lg text-[#4B0000] h-12 w-72 rounded-xl font-semibold  hover:bg-[#4B0000] hover:text-white transition duration-150">Explore</button>
           </div>
           <div className="RecipeCard2 bg-[#3179FF] h-96 w-72 sm:w-80 rounded-2xl shadow-xl transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl flex flex-col items-center justify-center relative">
             <div className="h-36 w-72 bg-[#000A4B] p-3 rounded-2xl -translate-y-[105px] flex items-start justify-start">
@@ -35,7 +35,7 @@ const RecipePage = () => {
                 {recipes[1].title}
               </h1>
             </div>
-            <button className="font-semibold absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-xl h-12 w-72 shadow">
+            <button className="font-semibold absolute bottom-4 left-1/2 transform -translate-x-1/2 text-lg bg-white text-[#000A4B] px-4 py-2 rounded-xl h-12 w-72 shadow hover:bg-[#000A4B] hover:text-white transition duration-150">
               Explore
             </button>
           </div>
@@ -45,7 +45,7 @@ const RecipePage = () => {
                 {recipes[2].title}
               </h1>
             </div>
-            <button className="font-semibold absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded-xl shadow bg-white h-12 w-72">Explore</button>
+            <button className="font-semibold absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded-xl shadow text-[#464B00] bg-white h-12 w-72 hover:bg-[#464B00] hover:text-white transition duration-150">Explore</button>
 
           </div>
         </div>
